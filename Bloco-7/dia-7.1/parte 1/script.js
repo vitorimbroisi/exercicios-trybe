1ee474a6daa2f608d9ddbd0b0fe20db8671614d4
Vitor <imbroisi.vitor@gmail.com>
2021-11-06 21:56:53 -0300

    dia 7.1

---
+++ Bloco-7/dia-7.1/parte 1/script.js
@@ -0,0 +1,26 @@
+// testingScope = escopo => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
+
+// testingScope(true);
+
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+oddsAndEvens.sort(function(a, b) {
+  return a - b;
+});
+console.log(oddsAndEvens);
+
+// Seu código aqui.
+
+// console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+
+
