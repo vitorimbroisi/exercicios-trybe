211bbd2688cfb92cdcf8058bd3c2207811e531a0
Seu nome <imbroisi.vitor@gmail.com>
2022-04-06 14:35:27 -0300

    finaliza dia 10.1

---
+++ bloco-10/dia-10.1/exercicios-dia-10.1/exercicios-10.1.2/exercicios-10.1.2/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import Header from './Header';
+import './App.css';
+import Content from './Content';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Content/>
+    </div>
+  );
+}
+
+export default App;
