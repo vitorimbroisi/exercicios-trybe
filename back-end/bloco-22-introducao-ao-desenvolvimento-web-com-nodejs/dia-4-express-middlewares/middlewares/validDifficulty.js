5ebd98270bd0a40bce94d773c8e9313b911a22b2
vitorimbroisi <imbroisi.vitor@gmail.com>
2022-08-30 21:56:15 -0300

    finaliza exercicio 1, dia 22.4

---
+++ back-end/bloco-22-introducao-ao-desenvolvimento-web-com-nodejs/dia-4-express-middlewares/middlewares/validDifficulty.js
@@ -0,0 +1,10 @@
+module.exports = (req, res, next) => {
+  const { difficulty } = req.body;
+
+  if(difficulty ==! 'Fácil' || difficulty ==! 'Médio' || difficulty ==! 'Difícil') {
+    return res.status(400).json(
+      { message: 'O campo difficulty deve ser \'Fácil\', \'Médio\' ou \'Difícil\'' },
+    );
+  }
+  next();
+};
